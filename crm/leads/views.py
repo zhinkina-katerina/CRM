3586ebc178135d22a4d037e4e3cb660d5ab68695
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-02-05 12:40:42 +0200

    fix(view): fix case sensitive search

---
+++ crm/leads/views.py
@@ -0,0 +1,53 @@
+from .models import Order
+from django.shortcuts import render
+from django.db.models import Q
+from leads.utilits import edit_query_to_dict
+
+
+def order_list(request):
+    orders = Order.objects.prefetch_related('product_set', 'delivery_set').all()
+    search_object = request.GET.get('search', '').lower()
+    if search_object:
+
+        orders = orders.filter(
+            Q(prom_id__iregex=search_object)|
+            Q(customer__fullname__iregex=search_object)|
+            Q(product__name__iregex=search_object))
+
+
+
+    result = []
+    for order in orders:
+        products = edit_query_to_dict(order.product_set)
+        delivery = edit_query_to_dict(order.delivery_set)
+        picture = list(products.values())[0]['image']
+
+
+
+        result.append({'prom_id': order.prom_id,
+                       'date_creation': order.date_creation,
+                       'ttn':order.ttn,
+                       'status':order.status,
+                       'customer': order.customer,
+                       'payment_name': order.payment_name,
+                       'total_price': order.total_price,
+                       'picture': picture,
+
+                       'products': products,
+                       'delivery':delivery,
+
+
+                       })
+
+    return render( request, 'order_list.html', {'orders': result})
+
+def order_details(request, id):
+    order = Order.objects.prefetch_related('product_set', 'delivery_set').get(prom_id=id)
+    products = edit_query_to_dict(order.product_set)
+    delivery = edit_query_to_dict(order.delivery_set)
+
+    return render(request, 'order_details.html', {'order': order,
+                                                  'products': products,
+                                                  'delivery':delivery
+                                                  })
+
