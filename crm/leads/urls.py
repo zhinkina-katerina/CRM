ba2078123f2e22c7672b8aebdedc21e5df9e39f9
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-02-08 13:33:39 +0200

    feat(form):add checkbox disloyal client - add IsDisloyalCustomer checkbox - add jQuery file set_disloyal_client.js - change checkbox disloyal client in order_details.html - add path set_disloyal_client - add handling view set_disloyal_client

--- crm/leads/urls.py
+++ crm/leads/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('order/<int:id>/', views.order_details, name='order_details'),
     path('search/', views.order_list, name='search'),
     path('ajax/set_new_ttn/', views.set_new_value, name='set_new_value'),
-
     path('ajax/set_status_order/', views.set_status_order, name='set_status_order'),
+    path('ajax/set_disloyal_client/', views.set_disloyal_client, name='set_disloyal_client'),
 
 ]
