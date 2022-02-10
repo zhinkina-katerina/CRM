1e9fa72bc4d591064cbe7a1295250cffd3da494c
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-02-10 18:25:43 +0200

    feat(module): refactor handler_orders.py

--- crm/leads/handler_orders.py
+++ crm/leads/handler_orders.py
@@ -6,48 +6,68 @@ ORDER_STATUSES = ['new', 'paid', 'received']
 
 
 class HandlerOrders:
+    def __init__(self):
+        self.fullname = None
+        self.city = None
+        self.post_office_number = None
+        self.payment_name = None
+        self.email = None
+        self.status = None
 
     def handle_order(self, order):
-        fullname = order.get('client_first_name', '') + ' ' + \
-                   order.get('client_second_name', '') + ' ' + \
-                   order.get('client_last_name', '')
+        client_first_name = order['client_first_name']
+        if not client_first_name: client_first_name = ''
+        client_second_name = order["client_second_name"]
+        if not client_second_name: client_second_name = ''
+        client_last_name = order['client_last_name']
+        if not client_last_name: client_last_name = ''
+
+        self.fullname = (client_first_name + ' ' +
+                         client_second_name + ' ' +
+                         client_last_name)
 
         address = order.get('delivery_address', '').split(",")
-        city = address[0]
-        post_office_number = address[1]
-        return fullname, city, post_office_number
+        self.city = address[0]
+        self.post_office_number = address[1]
+
+        self.payment_name = order['payment_option']['name']
+        if not self.payment_name:
+            self.payment_name = 'Способ оплаты не указан"'
+
+        self.email = order['email']
+        if not self.email:
+            self.email = 'Почта не указана'
+
+        self.status = {'received': 'In_process',
+                       'paid': 'Done',
+                       'new': 'New',
+                       }[order['status']]
 
     def create_object_order(self):
         order_list = prom_client.get_order_list()
-        for order in order_list['orders']:
 
+        for order in order_list['orders']:
             if order['status'] not in ORDER_STATUSES:
                 continue
 
-            try:
-                Order.objects.get(prom_id=order['id'])
+            if Order.objects.filter(prom_id=order['id']).first():
                 continue
-            except Exception('Данный заказ есть в базе'):
-                pass
 
-            fullname, city, post_office_number = self.handle_order(order=order)
+            self.handle_order(order=order)
 
-            try:
-                customer = Customer.objects.get(phone_number=order['phone'])
-            except:
-                customer = Customer.objects.create(fullname=fullname,
-                                                   email=order.get('email', ''),
-                                                   phone_number=order['phone'])
-            status = {'received': 'Принят',
-                      'paid': 'Оплачен',
-                      }[order['status']]
+            customer = Customer.objects.filter(phone_number=order['phone']).first()
+            if not customer:
+                customer = Customer.objects.create(fullname=self.fullname,
+                                                   email=self.email,
+                                                   phone_number=order['phone'],
+                                                   )
 
             order_prom = Order.objects.create(prom_id=order['id'],
                                               date_creation=order['date_created'][:10],
-                                              status=status,
+                                              status=self.status,
                                               customer=customer,
                                               source=order['source'],
-                                              payment_name=order.get('payment_option', '').get('name', ''),
+                                              payment_name=self.payment_name,
                                               total_price=order['price']
                                               )
 
@@ -58,7 +78,9 @@ class HandlerOrders:
                 warehouse = {
                     '01-': 'ToysWorld',
                     '02-': 'OnlyToys',
-                    '03-': 'JustToys'
+                    '03-': 'JustToys',
+                    '04-': 'NewToys',
+                    '05-': 'StarToys',
                 }[sku[:3]]
 
                 Product.objects.create(order=order_prom,
@@ -68,10 +90,9 @@ class HandlerOrders:
                                        sku=sku,
                                        image=product['image'].replace('w100_h100', 'w300_h300'),
                                        warehouse=warehouse,
-
                                        )
 
             Delivery.objects.create(order=order_prom,
-                                    city=city,
-                                    post_office_number=post_office_number,
+                                    city=self.city,
+                                    post_office_number=self.post_office_number,
                                     )
